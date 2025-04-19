fc6ea1ad8017304d728c159f2190a862b7b9f96d
DIVYAM VERMA <113888422+divyam8175@users.noreply.github.com>
2025-04-19 14:48:13 +0530

    Time: 3 ms (48.55%) | Memory: 12.1 MB (16.70%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.cpp
+++ 273-integer-to-english-words/integer-to-english-words.cpp
@@ -1,89 +1,41 @@
 class Solution {
 public:
+    unordered_map<int,string> belowTen {
+        {0,""}, {1,"One"}, {2,"Two"},   {3,"Three"}, {4,"Four"},
+        {5,"Five"},{6,"Six"}, {7,"Seven"},{8,"Eight"},{9,"Nine"}
+    };
+    unordered_map<int,string> belowTwenty {
+        {10,"Ten"},{11,"Eleven"},{12,"Twelve"},{13,"Thirteen"},{14,"Fourteen"},
+        {15,"Fifteen"},{16,"Sixteen"},{17,"Seventeen"},{18,"Eighteen"},{19,"Nineteen"}
+    };
+    unordered_map<int,string> belowHundred {
+        {2,"Twenty"},{3,"Thirty"},{4,"Forty"},{5,"Fifty"},
+        {6,"Sixty"},{7,"Seventy"},{8,"Eighty"},{9,"Ninety"}
+    };
+
+    string solve(int num) {
+        if (num < 10)
+            return belowTen[num];
+        if (num < 20)
+            return belowTwenty[num];
+        if (num < 100)
+            return belowHundred[num/10]
+                 + ((num%10) ? " " + belowTen[num%10] : "");
+        if (num < 1000)
+            return solve(num/100) + " Hundred"
+                 + ((num%100) ? " " + solve(num%100) : "");
+        if (num < 1000000) 
+            return solve(num/1000) + " Thousand"
+                 + ((num%1000) ? " " + solve(num%1000) : "");
+        if (num < 1000000000) 
+            return solve(num/1000000) + " Million"
+                 + ((num%1000000) ? " " + solve(num%1000000) : "");
+        return solve(num/1000000000) + " Billion"
+             + ((num%1000000000) ? " " + solve(num%1000000000) : "");
+    }
+
     string numberToWords(int num) {
-        if (num == 0)
-            return "Zero";
-
-        vector<string> place{"Billion", "Million", "Thousand", ""};
-
-        vector<string> number{"",        "One",       "Two",      "Three",
-                              "Four",    "Five",      "Six",      "Seven",
-                              "Eight",   "Nine",      "Ten",      "Eleven",
-                              "Twelve",  "Thirteen",  "Fourteen", "Fifteen",
-                              "Sixteen", "Seventeen", "Eighteen", "Nineteen",
-                              "Twenty"};
-
-        reverse(place.begin(), place.end());
-
-        unordered_map<int, string> mpp;
-        for (int i = 0; i < number.size(); i++) {
-            mpp[i] = number[i];
-        }
-        mpp[30] = "Thirty";
-        mpp[40] = "Forty";
-        mpp[50] = "Fifty";
-        mpp[60] = "Sixty";
-        mpp[70] = "Seventy";
-        mpp[80] = "Eighty";
-        mpp[90] = "Ninety";
-        int index = 0;
-        string ans = "";
-        while (num > 0) {
-            int rem = num % 1000;
-            num = num / 1000;
-            int a = rem % 100;
-            rem = rem / 100;
-            int c = rem % 10;
-            rem = rem / 10;
-            string res = "";
-            // cout<<a<<" "<<c<<endl;
-            if (a != 0) {
-                if (a < 10) {
-                    if (res.size() != 0)
-                        res = mpp[a] + " " + res;
-                    else
-                        res = mpp[a] + res;
-                } else if (a <= 20) {
-                    if (res.size() != 0)
-                        res = mpp[a] + " " + res;
-                    else
-                        res = mpp[a] + res;
-                } else {
-                    if (a % 10 == 0) {
-                        if (res.size() != 0)
-                            res = mpp[a] + " " + res;
-                        else
-                            res = mpp[a] + res;
-                    } else {
-                        int b = a % 10;
-                        a = a / 10;
-                        if (res.size() != 0 && b != 0)
-                            res = mpp[a * 10] + " " + mpp[b] + " " + res;
-                        else
-                            res = mpp[a * 10] + " " + mpp[b] + res;
-                    }
-                }
-            }
-            if (c != 0) {
-                if (res.size() != 0)
-                    res = mpp[c] + " " + "Hundred" + " " + res;
-                else
-                    res = mpp[c] + " " + "Hundred" + res;
-            }
-            // cout << res << endl;
-            if (res.size() != 0)
-                res = res + " " + place[index];
-            // cout << res << endl << endl;
-            if (ans.size() != 0 && res.size()!=0)
-                ans = res + " " + ans;
-            else
-                ans = res + ans;
-            cout<<ans<<endl;
-            index++;
-        }
-        int j = ans.size() - 1;
-        while (j >= 0 && ans[j] == ' ')
-            j--;
-        return ans.substr(0, j + 1);
+        if (num == 0) return "Zero";
+        return solve(num);
     }
 };
