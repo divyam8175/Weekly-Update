5e028d638a93e1b089b1f8ce4ef3bc4c4cbdd86a
DIVYAM VERMA <113888422+divyam8175@users.noreply.github.com>
2025-07-27 23:25:17 +0530

    Time: 7 ms (22.94%) | Memory: 13.6 MB (20.00%) - LeetSync

--- 929-groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.cpp
+++ 929-groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.cpp
@@ -6,16 +6,10 @@ static bool customCompare(const char &a,const char &b) {
 
 void sortOddIndexChars(string& s) {
     vector<char> oddChars;
-
-    // Extract odd-indexed characters
     for (int i = 1; i < s.length(); i += 2) {
         oddChars.push_back(s[i]);
     }
-
-    // Sort with custom comparator
     sort(oddChars.begin(), oddChars.end(), customCompare);
-
-    // Put sorted characters back at odd indices
     int idx = 0;
     for (int i = 1; i < s.length(); i += 2) {
         s[i] = oddChars[idx++];
@@ -23,16 +17,10 @@ void sortOddIndexChars(string& s) {
 }
 void sortEvenIndexChars(string& s) {
     vector<char> evenChars;
-
-    // Extract odd-indexed characters
     for (int i = 0; i < s.length(); i += 2) {
         evenChars.push_back(s[i]);
     }
-
-    // Sort with custom comparator
     sort(evenChars.begin(), evenChars.end(), customCompare);
-
-    // Put sorted characters back at odd indices
     int idx = 0;
     for (int i = 0; i < s.length(); i += 2) {
         s[i] = evenChars[idx++];
