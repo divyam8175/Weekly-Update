c1ffd224da3a1abe7cb2c1e9ff47ab2263c0f023
DIVYAM VERMA <113888422+divyam8175@users.noreply.github.com>
2026-03-01 17:06:51 +0530

    Time: 4 ms (27.16%) | Memory: 21.1 MB (15.16%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        const int size = isConnected.size();
+        unordered_map<int, vector<int>> mpp;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < isConnected[i].size(); j++) {
+                if (isConnected[i][j])
+                    mpp[i].push_back(j);
+            }
+        }
+        int count = 0;
+        vector<int> vis(size, 0);
+        for (int i = 0; i < isConnected.size(); i++) {
+            if (vis[i] == 1)
+                continue;
+            queue<int> q;
+            q.push(i);
+            vis[i] = 1;
+            while (!q.empty()) {
+                int u = q.front();
+                q.pop();
+
+                for (auto it : mpp[u]) {
+                    if (vis[it] == 0) {
+                        vis[it] = 1;
+                        q.push(it);
+                    }
+                }
+            }
+            count++;
+        }
+        return count;
+    }
+};
